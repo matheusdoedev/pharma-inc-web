79e55ca3d301322c27fb2e4537fd1a57393ba52c
matheusdoedev <matheusdoe.dev@gmail.com>
2021-09-17 14:39:04 -0300

    refactor: working in Dashboard Layout
    wrapper and content styled components created

--- src/layouts/DashboardLayout/index.tsx
+++ src/layouts/DashboardLayout/index.tsx
@@ -1,11 +1,20 @@
 import { ReactElement, ReactNode } from 'react';
 
+import { Sidebar } from '~/components';
+
+import * as S from './styles';
+
 interface IDashboardLayout {
   children?: ReactNode;
 }
 
 function DashboardLayout({ children }: IDashboardLayout): ReactElement {
-  return <main>{children}</main>;
+  return (
+    <S.Wrapper>
+      <Sidebar />
+      <S.Content>{children}</S.Content>
+    </S.Wrapper>
+  );
 }
 
 DashboardLayout.defaultProps = {
---
+++ src/layouts/DashboardLayout/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  width: 100%;
+  height: 100vh;
+`;
+
+export const Content = styled.section`
+  overflow-y: scroll;
+`;
