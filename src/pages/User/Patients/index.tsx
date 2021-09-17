9a373a7577f1203b2ed405d78af010cae459e0ca
matheusdoedev <matheusdoe.dev@gmail.com>
2021-09-17 14:39:24 -0300

    refactor: working in Patients page
    filters section created

--- src/pages/User/Patients/index.tsx
+++ src/pages/User/Patients/index.tsx
@@ -1,11 +1,36 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 
+import { Input, Select } from '~/components';
 import { DashboardLayout } from '~/layouts';
 
+import * as S from './styles';
+
 function Patients(): ReactElement {
+  const [searchFilter, setSearchFilter] = useState<string>('');
+  const [sortFilter, setSortFilter] = useState<string>('');
+
   return (
     <DashboardLayout>
-      <h1>Hello World</h1>
+      <S.Wrapper>
+        <S.Filters>
+          <Select
+            name="sort_filter"
+            label="Ordenar por"
+            placeholder="Ordenar por"
+            value={sortFilter}
+            setValue={setSortFilter}
+            containerStyle={{ maxWidth: '21.8rem' }}
+          />
+          <Input
+            name="name_filter"
+            label="Procurar"
+            placeholder="Filtrar por nome ou nacionalidade"
+            value={searchFilter}
+            setValue={setSearchFilter}
+            containerStyle={{ maxWidth: '38rem' }}
+          />
+        </S.Filters>
+      </S.Wrapper>
     </DashboardLayout>
   );
 }
---
+++ src/pages/User/Patients/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  padding: ${({ theme }) => theme.spacings.lg};
+`;
+
+export const Filters = styled.section`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: ${({ theme }) => theme.spacings.sm};
+`;
