0f3e3afb612de5e62d4bd1907a286ececd6d754a
matheusdoedev <matheusdoe.dev@gmail.com>
2021-09-19 13:10:07 -0300

    refactor: working in Patients page

--- src/pages/User/Patients/index.tsx
+++ src/pages/User/Patients/index.tsx
@@ -1,13 +1,38 @@
-import { ReactElement, useState } from 'react';
+import { ReactElement, useState, useCallback, useEffect } from 'react';
+
+import { TableCell, TableRow } from '@material-ui/core';
 
 import { Input, Select, Table } from '~/components';
+import { usePatients } from '~/hooks/Patients';
 import { DashboardLayout } from '~/layouts';
+import { IPageRequestOptions } from '~/models/Common';
+import { PatientsService } from '~/services';
 
 import * as S from './styles';
 
 function Patients(): ReactElement {
   const [searchFilter, setSearchFilter] = useState<string>('');
   const [sortFilter, setSortFilter] = useState<string>('');
+  const [pageOptions, setPageOptions] = useState<IPageRequestOptions>({
+    page: 1,
+    results: 50,
+  });
+
+  const { patients, setPatients } = usePatients();
+
+  const handleGetPatients = useCallback(async () => {
+    try {
+      const response = await PatientsService.getPatients(pageOptions);
+
+      setPatients(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [pageOptions, setPatients]);
+
+  useEffect(() => {
+    handleGetPatients();
+  }, [handleGetPatients]);
 
   return (
     <DashboardLayout>
@@ -30,13 +55,16 @@ function Patients(): ReactElement {
             containerStyle={{ maxWidth: '38rem' }}
           />
         </S.Filters>
-        <Table
-          columnsName={['Nome', 'Genero']}
-          data={[
-            { name: 'Matheus', genre: 'Masculino' },
-            { name: 'Jennifer', genre: 'Feminino' },
-          ]}
-        />
+        <Table columnsName={['Nome', 'Genero']}>
+          {patients?.results.map(patient => (
+            <TableRow>
+              <TableCell component="th" scope="row">
+                {patient.name.first} {patient.name.last}
+              </TableCell>
+              <TableCell>{patient.gender}</TableCell>
+            </TableRow>
+          ))}
+        </Table>
       </S.Wrapper>
     </DashboardLayout>
   );
